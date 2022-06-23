7e0134f5766ded822dda50446221ee48348fd2fa
zhouguilin <zhouguilin@hnnewsoft.com>
2022-06-23 16:50:22 +0800

    fix: 省市选择器

--- src/pages/settlement.vue
+++ src/pages/settlement.vue
@@ -26,8 +26,10 @@
         <div style="font-size: 16px; font-weight: 500; margin-bottom: 10px">
           收货人信息
         </div>
-        <div style="float: right;margin-top: -40px;">
-          <el-button type="text"><i class="el-icon-plus"></i>新增收货地址</el-button>
+        <div style="float: right; margin-top: -40px">
+          <el-button type="text" @click="add"
+            ><i class="el-icon-plus"></i>新增收货地址</el-button
+          >
         </div>
         <div>
           <div
@@ -75,27 +77,94 @@
               默认地址
             </div>
             <div
-              style="height: 35px; line-height: 35px;margin-left: 100px;"
+              style="height: 35px; line-height: 35px; margin-left: 100px"
               v-if="item.default === false"
             >
               <el-button type="text">设置为默认地址</el-button>
             </div>
-            <div style="height: 35px; line-height: 35px;margin-left: 20px;">
-              <i style="cursor: pointer;" class="el-icon-edit"></i>
+            <div style="height: 35px; line-height: 35px; margin-left: 20px">
+              <i style="cursor: pointer" class="el-icon-edit"></i>
             </div>
             <div
-              style="height: 35px; line-height: 35px;margin-left: 20px;"
+              style="height: 35px; line-height: 35px; margin-left: 20px"
               v-if="item.default !== true"
             >
-              <i style="cursor: pointer;" class="el-icon-delete"></i>
+              <i style="cursor: pointer" class="el-icon-delete"></i>
             </div>
           </div>
         </div>
         <div class="line"></div>
+        <div style="margin-bottom: 30px">
+          <div style="font-size: 16px; font-weight: 500; margin-bottom: 10px">
+            支付方式
+          </div>
+          <div style="display: flex">
+            <div class="paytype"><i class="el-icon-s-shop"></i>货到付款</div>
+            <div class="paytype">
+              <i class="el-icon-full-screen"></i>在线支付
+            </div>
+            <div class="paytype"><i class="el-icon-bank-card"></i>对公转账</div>
+          </div>
+        </div>
         <div class="line"></div>
+        <div style="margin-bottom: 30px">
+          <div style="font-size: 16px; font-weight: 500; margin-bottom: 10px">
+            送货清单
+          </div>
+          <div
+            style="
+              border: 1px solid #f7f5f5;
+              background: #f7f5f5;
+              border-radius: 5px;
+            "
+          >
+            11
+          </div>
+        </div>
         <div class="line"></div>
       </div>
     </div>
+    <el-dialog
+      title="新增收货人信息"
+      :visible.sync="dialogVisible"
+      width="650px"
+      :before-close="handleClose"
+    >
+      <div style="padding: 0 30px">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <el-form-item label="所在区域" prop="cities">
+            <el-cascader
+              size="large"
+              :options="options"
+              v-model="ruleForm.cities"
+              @change="handleChange"
+            >
+            </el-cascader>
+          </el-form-item>
+          <el-form-item label="收货人" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="详细地址" prop="detail">
+            <el-input v-model="ruleForm.detail"></el-input>
+          </el-form-item>
+          <el-form-item label="手机号" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="submitForm('ruleForm')"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
     <Footer></Footer>
   </div>
 </template>
@@ -103,16 +172,50 @@
 <script>
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { provinceAndCityData } from "element-china-area-data";
 export default {
   components: {
     Header,
     Footer,
   },
   data() {
+    let validatorPhone = function (phone, value, callback) {
+      let reg =
+        /^(((13[0-9]{1})|(15[0-9]{1})|(16[0-9]{1})|(17[3-8]{1})|(18[0-9]{1})|(19[0-9]{1})|(14[5-7]{1}))+\d{8})$/;
+      if (value === "") {
+        callback(new Error("手机号不能为空"));
+      } else if (!reg.test(value)) {
+        callback(new Error("手机号格式错误"));
+      } else {
+        callback();
+      }
+    };
     return {
+      options: provinceAndCityData,
       ProductData: [1, 2, 3, 4, 5, 6],
+      dialogVisible: false,
+      ruleForm: {
+        id: 0,
+        name: "",
+        cities: "",
+        detail: "",
+        phone: "",
+      },
+      rules: {
+        name: [
+          { required: true, message: "请输入收货人姓名", trigger: "blur" },
+        ],
+        cities: [
+          { required: true, message: "请选择所在省市“", trigger: "blur" },
+        ],
+        detail: [
+          { required: true, message: "请输入详细地址", trigger: "blur" },
+        ],
+        phone: [{ required: true, validator: validatorPhone, trigger: "blur" }],
+      },
       addressList: [
         {
+          id: 1,
           name: "周桂林",
           cities: "北京市 朝阳区",
           detail: "小关安苑北里安苑北里小区 19号楼 1708",
@@ -120,6 +223,7 @@ export default {
           default: true,
         },
         {
+          id: 2,
           name: "周大林",
           cities: "北京市 朝阳区",
           detail: "小关安苑北里安苑北里小区 19号楼 1708",
@@ -127,6 +231,7 @@ export default {
           default: false,
         },
         {
+          id: 3,
           name: "周小林",
           cities: "北京市 朝阳区",
           detail: "小关安苑北里安苑北里小区 19号楼 1708",
@@ -143,6 +248,27 @@ export default {
   methods: {
     onSelect(type) {
       this.curSelect = type;
+    },
+    add() {
+      this.dialogVisible = true;
+    },
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then((_) => {
+          done();
+        })
+        .catch((_) => {});
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+        } else {
+          return false;
+        }
+      });
+    },
+    handleChange(value) {
+      console.log(value);
     },
   },
 };
@@ -178,5 +304,14 @@ export default {
 .line {
   border-bottom: 1px solid #009866;
   margin-bottom: 20px;
+}
+.paytype {
+  border: 1px solid #009866;
+  padding: 5px 20px;
+  margin-right: 50px;
+  cursor: pointer;
+}
+.paytype:hover {
+  color: #009866;
 }
 </style>
